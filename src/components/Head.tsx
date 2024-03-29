59b07d35cc08de09bd6bf5856f696f801695af89
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-03-29 19:34:54 +0900

    feat: update favicon

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,7 +6,10 @@ export const Head: FC = (props) => {
     <head>
       <title>{props.metadata.title}</title>
       <Style />
-      <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👨‍💻</text></svg>"
+      ></link>
       <link rel="sitemap" href="/sitemap.xml" />
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
